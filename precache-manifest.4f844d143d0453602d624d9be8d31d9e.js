db59a49517881c359058afe1defa36239a660337
Gunil <gunil.na1@gmail.com>
2020-10-05 14:52:44 +0200

    Updates

---
+++ precache-manifest.4f844d143d0453602d624d9be8d31d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035e11b60e31fb4c5dbc5fbe491f61ec",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "bf637dfe1fba510a2aea",
+    "url": "/react-monster-rolodex/static/css/main.1ec23593.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf637dfe1fba510a2aea",
+    "url": "/react-monster-rolodex/static/js/main.55c3db0f.chunk.js"
+  },
+  {
+    "revision": "e5eb423b2a2cc1d9f46f",
+    "url": "/react-monster-rolodex/static/js/runtime-main.856af338.js"
+  }
+]);
